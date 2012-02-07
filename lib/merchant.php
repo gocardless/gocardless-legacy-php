322b136655845eebb440bc7b9c4c118003dbf1c3
David Haywood Smith <davehs@gmail.com>
2012-02-07 15:04:03 +0000

    Merchant subclass methods

--- lib/merchant.php
+++ lib/merchant.php
@@ -6,12 +6,24 @@ class Merchant {
 		$this->gocardless = $gocardless;
 	}
 	
-	public function subscriptions($params) {
-		
-		return $params['id'];
-		//return $this->gocardless->app_identifier;
-		//return parent::generate_url('subscription', $payment_details);
-		
+	public function subscriptions($merchant_id) {
+		return $this->gocardless->fetch_resource("merchants/$merchant_id/subscriptions");
+	}
+	
+	public function pre_authorizations($merchant_id) {
+		return $this->gocardless->fetch_resource("merchants/$merchant_id/pre_authorizations");
+	}
+	
+	public function users($merchant_id) {
+		return $this->gocardless->fetch_resource("merchants/$merchant_id/users");
+	}
+	
+	public function bills($merchant_id) {
+		return $this->gocardless->fetch_resource("merchants/$merchant_id/bills");
+	}
+	
+	public function payments($merchant_id) {
+		return $this->gocardless->fetch_resource("merchants/$merchant_id/payments");
 	}
 	
 }
