e9231b60cb82a34eeceb61e20525418057cf8586
David Haywood Smith <davehs@gmail.com>
2012-02-07 13:51:11 +0000

    Added (working) test file

---
+++ tests.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'gocardless.php';
+
+class GoCardlessTest extends PHPUnit_Framework_TestCase {
+
+    function testThatItWorks() {
+        self::assertTrue(false);
+    }
+
+    function testThatItDoesntWork() {
+        self::assertTrue(true);
+    }
+	
+}
+
+?>
