a34969fce9e87471ccd5f9d370dbf0f4a0e852ae
David Haywood Smith <davehs@gmail.com>
2012-02-07 14:27:14 +0000

    Whitespace

--- tests.php
+++ tests.php
@@ -3,11 +3,11 @@
 require_once 'gocardless.php';
 
 class GoCardlessTest extends PHPUnit_Framework_TestCase {
-
+	
     function testThatItWorks() {
         self::assertTrue(false);
     }
-
+	
     function testThatItDoesntWork() {
         self::assertTrue(true);
     }
