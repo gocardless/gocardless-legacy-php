417d848e6daab6b75dd2325c9d9bb0ffc05f6cbc
David Haywood Smith <davehs@gmail.com>
2012-02-07 14:42:33 +0000

    Improved instantiation of subclasses

--- lib/merchant.php
+++ lib/merchant.php
@@ -2,33 +2,14 @@
 
 class Merchant {
 	
-	public function __construct($gocardless) {
-		
-		echo '<pre>';
-		print_r($gocardless);
-		
-		if (!isset($gocardless)) {
-			// Fail, no config vars
-		}
-		
-		foreach ($gocardless as $key => $value) {
-			$this->{$key} = $value;
-		}
-		
-		// If environment is not set then default to production
-		if (!isset($gocardless->environment)) {
-			$this->environment = 'production';
-		}
-		
-		// Set base_url based on environment
-		$this->base_url = $gocardless->base_urls[$gocardless->environment];
-		
+	public function __construct(&$gocardless) {
+		$this->gocardless = $gocardless;
 	}
 	
 	public function subscriptions($params) {
 		
 		return $params['id'];
-		//return $this->app_identifier;
+		//return $this->gocardless->app_identifier;
 		//return parent::generate_url('subscription', $payment_details);
 		
 	}
