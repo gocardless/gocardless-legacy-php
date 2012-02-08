24a82e0941d95d284d1d4d8dc44c2d7b8e0ee631
David Haywood Smith <davehs@gmail.com>
2012-02-08 17:44:28 +0000

    Improved testing environment and added first exception test

--- tests.php
+++ tests.php
@@ -4,9 +4,12 @@ require_once 'gocardless.php';
 
 class GoCardlessTest extends PHPUnit_Framework_TestCase {
 	
+	public $config;
+	
 	public function setUp() {
 		
-		$gocardless_config = array(
+		$this->config = array(
+			'merchant_id'	=> '258584',
 			'app_id'		=> 'eCxrcWDxjYsQ55zhsDTgs6VeKf6YWZP7be/9rY0PGFbeyqmLJV6k84SUQdISLUhf',
 			'app_secret'	=> '2utXOc65Hy9dolp3urYBMoIN0DM11Q9uuoboFDkHY3nzsugqcuzD1FuJYA7X9TP+',
 			'access_token'	=> '+vJh7dkHLr5rbdqBLlRk3dPALyn0uvAKTMvRnfWOAKcQ6WRCx/QGsdOefGqEs6h6',
@@ -14,22 +17,38 @@ class GoCardlessTest extends PHPUnit_Framework_TestCase {
 			'redirect_uri'	=> 'http://localhost:8888/demo.php'
 		);
 		
-		$this->_GoCardless = new GoCardless($gocardless_config);
-		
 	}
 	
 	public function testBaseUrlInSandbox() {
-		$_GoCardless->base_url = $_GoCardless->base_urls['sandbox'];
-		$this->assertEquals('https://sandbox.gocardless.com', $_GoCardless->base_url);
+		
+		$this->config['environment'] = 'sandbox';
+		
+		$this->_GoCardless = new GoCardless($this->config);
+		
+		$this->assertEquals('https://sandbox.gocardless.com', $this->_GoCardless->base_url);
+		
 	}
 	
-    function testThatItWorks() {
-        self::assertTrue(false);
-    }
+	public function testBaseUrlInProduction() {
+		
+		$this->config['environment'] = 'production';
+		
+		$this->_GoCardless = new GoCardless($this->config);
+		
+		$this->assertEquals('https://gocardless.com', $this->_GoCardless->base_url);
+		
+	}
 	
-    function testThatItDoesntWork() {
-        self::assertTrue(true);
-    }
+	/**
+     * @expectedException GoCardlessClientException
+     */
+	public function testExceptionWithNoAppId() {
+		
+		$this->config['app_id'] = null;
+		
+		$this->_GoCardless = new GoCardless($this->config);
+		
+	}
 	
 }
 
