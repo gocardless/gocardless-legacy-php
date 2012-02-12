9e6a6f7f5cd28d4bf63316b23e80ce6384586acf
David Haywood Smith <davehs@gmail.com>
2012-02-12 22:33:21 +0000

    Exception comments

--- lib/exceptions.php
+++ lib/exceptions.php
@@ -1,17 +1,32 @@
 <?php
 
+/**
+ * Throw an exception relating to the client object
+ *
+ * @return exception
+ */
 class GoCardlessClientException extends Exception {
 	public function __construct($description = 'Unknown client error') {
 		parent::__construct($description);
 	}
 }
 
+/**
+ * Throw an exception relating to the arguments used in a function
+ *
+ * @return exception
+ */
 class GoCardlessArgumentsException extends Exception {
 	public function __construct($description = 'Unknown argument error') {
 		parent::__construct($description);
 	}	
 }
 
+/**
+ * Throw an exception relating to the GoCardless API
+ *
+ * @return exception
+ */
 class GoCardlessApiException extends Exception {
 	public function __construct($description = 'Unknown error', $code = 0) {
 		if (empty($description)) {
