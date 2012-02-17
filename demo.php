4c65ad8321b202267501f4f63cdb15efaf3d8e1b
David Haywood Smith <davehs@gmail.com>
2012-02-17 17:11:06 +0000

    Clearer comments and hidden ids

--- demo.php
+++ demo.php
@@ -2,22 +2,21 @@
 
 // First create your application in the GoCardless sandbox:
 // https://sandbox.gocardless.com
-// Then grab your application identifier and secret...
+// Then grab your application id and secret and paste them in below
+// You'll also need to to change the various ids throughout this demo
 
 // Include library
 include_once 'gocardless.php';
-
-// ...and paste them in here:
 
 // Sandbox
 GoCardless::$environment = 'sandbox';
 
 // Config vars
 $account_details = array(
-  'app_id'        => 'eCxrcWDxjYsQ55zhsDTgs6VeKf6YWZP7be/9rY0PGFbeyqmLJV6k84SUQdISLUhf',
-  'app_secret'    => '2utXOc65Hy9dolp3urYBMoIN0DM11Q9uuoboFDkHY3nzsugqcuzD1FuJYA7X9TP+',
-  'merchant_id'   => '258584',
-  'access_token'  => 'ShFGfoXO+GOKEiMeoeMPUvSTTq9HC0v3BRLXo8eSUifbZOtneYqqNECPa+QK6AdL'
+  'app_id'        => null,
+  'app_secret'    => null,
+  'merchant_id'   => null,
+  'access_token'  => null
 );
 // Initialize GoCardless
 GoCardless::setAccountDetails($account_details);
--- demo_partner.php
+++ demo_partner.php
@@ -11,7 +11,8 @@
  * Save access_token and merchant_id in your database against the current user
  * And use them to initialize GoCardless for that user
  *
- * NB. You can also paste in access_token and merchant_id below for testing
+ * NB. You can then paste in access_token and merchant_id below for testing
+ * And you may want to replace the ids in the various API calls too.
  *
  *
  * This page then does the following:
@@ -35,10 +36,10 @@ GoCardless::$environment = 'sandbox';
 
 // Config vars for your PARTNER account
 $account_details = array(
-  'app_id'        => 'eCxrcWDxjYsQ55zhsDTgs6VeKf6YWZP7be/9rY0PGFbeyqmLJV6k84SUQdISLUhf',
-  'app_secret'    => '2utXOc65Hy9dolp3urYBMoIN0DM11Q9uuoboFDkHY3nzsugqcuzD1FuJYA7X9TP+',
-  'access_token'  => '5KFYZpAGwBBQlYJxAz6evPFyTW3Nc6wPUo57kOf8L+fH6NS4ctX72W4BO/AR2uJA',
-  'merchant_id'   => '012GM2H8FA'
+  'app_id'        => null,
+  'app_secret'    => null,
+  'access_token'  => null,
+  'merchant_id'   => null
 );
 
 $gocardless_client = new GoCardless_Client($account_details);
@@ -56,10 +57,10 @@ if (isset($_GET['code'])) {
   $token = $gocardless_client->fetchAccessToken($params);
 
   $account_details = array(
-    'app_id'        => 'eCxrcWDxjYsQ55zhsDTgs6VeKf6YWZP7be/9rY0PGFbeyqmLJV6k84SUQdISLUhf',
-    'app_secret'    => '2utXOc65Hy9dolp3urYBMoIN0DM11Q9uuoboFDkHY3nzsugqcuzD1FuJYA7X9TP+',
-    'access_token'  => ' 5KFYZpAGwBBQlYJxAz6evPFyTW3Nc6wPUo57kOf8L+fH6NS4ctX72W4BO/AR2uJA',
-    'merchant_id'   => '012GM2H8FA'
+    'app_id'        => null,
+    'app_secret'    => null,
+    'access_token'  => null,
+    'merchant_id'   => null
   );
 
   $gocardless_client = new GoCardless_Client($account_details);
@@ -94,10 +95,10 @@ if ($account_details['access_token']) {
   echo '</pre></blockquote>';
 
   $account_details = array(
-    'app_id'        => 'eCxrcWDxjYsQ55zhsDTgs6VeKf6YWZP7be/9rY0PGFbeyqmLJV6k84SUQdISLUhf',
-    'app_secret'    => '2utXOc65Hy9dolp3urYBMoIN0DM11Q9uuoboFDkHY3nzsugqcuzD1FuJYA7X9TP+',
-    'access_token'  => 'ShFGfoXO+GOKEiMeoeMPUvSTTq9HC0v3BRLXo8eSUifbZOtneYqqNECPa+QK6AdL',
-    'merchant_id'   => '258584'
+    'app_id'        => null,
+    'app_secret'    => null,
+    'access_token'  => null,
+    'merchant_id'   => null
   );
 
   $gocardless_client2 = new GoCardless_Client($account_details);
