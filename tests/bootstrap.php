ab68179b0dc5aaf5e635f5aae60d0f2e10fdf72c
David Haywood Smith <davehs@gmail.com>
2012-03-27 13:35:08 +0100

    Fierce error reporting in tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ include_once('PHPUnit/Autoload.php');
 /**
  * Set error reporting and display errors settings.  You will want to change these when in production.
  */
-error_reporting(E_ALL);
+error_reporting(-1);
 ini_set('display_errors', true);
 
 include 'lib/GoCardless.php';
