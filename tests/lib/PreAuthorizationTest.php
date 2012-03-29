2e21334fbc1e2ac345bf41018d03debef0d9a279
David Haywood Smith <davehs@gmail.com>
2012-03-29 03:14:19 +0100

    Tests for cancelling a pre-auth

--- tests/lib/PreAuthorizationTest.php
+++ tests/lib/PreAuthorizationTest.php
@@ -59,4 +59,45 @@ class Test_PreAuthorization extends PHPUnit_Framework_TestCase {
 
 	}
 
+	/**
+	 * Tests for preauth->cancel
+	 */
+	public function testCancel() {
+
+    // Create a mock for the get method of GoCardless_Request
+    $get_stub = $this->getMock('GoCardless_Request', array('get'));
+
+    // Static dependency injection
+    GoCardless::setClass('Request', get_class($get_stub));
+
+    // Test finding a pre-authorization uses get
+		$get_stub->staticExpects($this->once())
+			->method('get')
+			->will($this->returnValue(array('id' => '123')));
+
+    // Load a mock pre-authorization
+    $preauth = GoCardless_PreAuthorization::find('123');
+
+    // Create a mock for the put method of GoCardless_Request
+    $put_stub = $this->getMock('GoCardless_Request', array('put'));
+
+    // Static dependency injection
+    GoCardless::setClass('Request', get_class($put_stub));
+
+    // Test that cancel a preauth uses put
+		$put_stub->staticExpects($this->once())
+			->method('put')
+			->will($this->returnValue(array('id' => '123')));
+
+    // Call the preauth->cancel() method
+    $result = $preauth->cancel();
+
+    // Test that cancel returns the preauth object
+    $this->assertInstanceOf('GoCardless_PreAuthorization', $result);
+
+    // Test that cancel returns the right id
+    $this->assertEquals('123', $result->id);
+
+	}
+
 }
