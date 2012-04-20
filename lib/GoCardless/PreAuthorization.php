52f46623b7b1cf54c2eec788642f05730b3cb239
David Haywood Smith <davehs@gmail.com>
2012-04-20 18:07:54 +0100

    Dynamic subresources for preauths

--- lib/GoCardless/PreAuthorization.php
+++ lib/GoCardless/PreAuthorization.php
@@ -37,6 +37,68 @@ class GoCardless_PreAuthorization {
       }
     }
 
+    // Check for subresources
+    if (isset($this->sub_resource_uris)) {
+
+      // Loop through each subresource loading it as appropriate object
+      foreach ($this->sub_resource_uris as $key => $value) {
+
+        // Generate subresource endpoint by snipping out the
+        // right part of the url
+        $endpoint = preg_replace('/api\/v[0-9]+\//', '',
+          parse_url($value, PHP_URL_PATH));
+
+        // Add params to endpoint
+        if (parse_url($value, PHP_URL_QUERY)) {
+          $endpoint .= '/?' . parse_url($value, PHP_URL_QUERY);
+        }
+
+        // Generate the class name
+        $class = 'GoCardless_' .
+          GoCardless_Utils::camelize(GoCardless_Utils::singularize($key));
+
+        // Create an array for the subresource
+        $this->$key = array();
+
+        // Query the API
+        foreach ($this->client->request('get', $endpoint) as $value) {
+
+          // Load each element into the appropriate class
+          $this->{$key}[] = new $class($this->client, $value);
+
+        }
+
+      }
+
+      // Unset the sub_resource_uris feild as we now have now loaded the
+      // subresources themselves
+      unset($this->sub_resource_uris);
+
+    }
+
+  }
+
+  /**
+   * This magic method is used to call subresources ie. merchant()->users()
+   *
+   * @param string $method The name of the method being called
+   *
+   * @return array The subresource index
+   */
+  public function __call($method, $params) {
+
+    // Check the subresource exists
+    if (isset($this->$method)) {
+
+      // Return the subresource
+      return $this->fetch_sub_resource($method, $params);
+
+    }
+
+    // Subresource doesn't exist so error out
+    $class = get_class($this);
+    trigger_error("Call to undefined method $class::$method()", E_USER_ERROR);
+
   }
 
   /**
@@ -68,6 +130,31 @@ class GoCardless_PreAuthorization {
     $endpoint = self::$endpoint . '/' . $id;
 
     return new self($client, $client->request('get', $endpoint));
+
+  }
+
+  /**
+   * Fetch an object's subresource from the API
+   *
+   * @param string $type The subresource to fetch
+   * @param array $params The params for the subresource query
+   *
+   * @return object The subresource object
+   */
+  public function fetch_sub_resource($type, $params = array()) {
+
+    $endpoint = self::$endpoint . '/' . $this->id . '/' . $type;
+
+    $class = 'GoCardless_' .
+      GoCardless_Utils::camelize(GoCardless_Utils::singularize($type));
+
+    $objects = array();
+
+    foreach ($this->client->request('get', $endpoint) as $value) {
+      $objects[] = new $class($this->client, $value);
+    }
+
+    return $objects;
 
   }
 
@@ -116,30 +203,6 @@ class GoCardless_PreAuthorization {
     $endpoint = self::$endpoint . '/' . $this->id . '/cancel';
 
     return new self($this->client, $this->client->request('put', $endpoint));
-
-  }
-
-  /**
-   * Fetch bills created under a pre-authorization from the API
-   *
-   * @param array $params Params to append to the query ie. for filtering
-   *
-   * @return array Array of bill objects
-   */
-  public function bills($params = array()) {
-
-    $params['source_id'] = $this->id;
-
-    $endpoint = GoCardless_Merchant::$endpoint . '/' . $this->merchant_id .
-      '/bills';
-
-    $objects = array();
-
-    foreach ($this->client->request('get', $endpoint, $params) as $value) {
-      $objects[] = new GoCardless_Bill($this->client, $value);
-    }
-
-    return $objects;
 
   }
 
