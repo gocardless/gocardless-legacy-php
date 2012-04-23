44fef69acf0768c4a83d9d751373d9b1ba782267
David Haywood Smith <davehs@gmail.com>
2012-04-23 15:30:41 +0100

    First tests for generate_query_string

--- tests/lib/UtilsTest.php
+++ tests/lib/UtilsTest.php
@@ -17,24 +17,6 @@ class Test_Utils extends PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	 * Check the signature is being generated correctly
-	 */
-	public function testGenerateSignature() {
-
-		$payment_details = array(
-		  'amount'          => '10.00',
-		  'interval_length' => 1,
-		  'interval_unit'   => 'month'
-		);
-
-		$sig = GoCardless_Utils::generate_signature($payment_details, $this->config['app_secret']);
-
-		// Check against a pre-built hash
-		$this->assertEquals('889b12a1aa31ca4c804d8554d4991fccc2d6d269bca4a20ecde6ef0cc20abc9c', $sig);
-
-	}
-
-	/**
 	 * Check camelize function
 	 */
 	public function testCamelize() {
@@ -57,5 +39,49 @@ class Test_Utils extends PHPUnit_Framework_TestCase {
     $this->assertEquals('cactus', $b);
 
   }
+
+	/**
+	 * Test generate_query_string joins items with equals
+	 */
+	public function testGenerateQueryStringJoinsItemsWithEquals() {
+
+    $params = array('a' => 'b');
+
+  	$return = GoCardless_Utils::generate_query_string($params);
+
+    $this->assertEquals("a=b", $return);
+
+  }
+
+	/**
+	 * Test generate_query_string joins pairs with ampersands
+	 */
+	public function testGenerateQueryStringJoinsPairsWithAmpersands() {
+
+    $params = array('a' => 'b', 'c' => 'd');
+
+  	$return = GoCardless_Utils::generate_query_string($params);
+
+    $this->assertEquals("a=b&c=d", $return);
+
+  }
+
+	/**
+	 * Check the signature is being generated correctly
+	 */
+	public function testGenerateSignature() {
+
+		$payment_details = array(
+		  'amount'          => '10.00',
+		  'interval_length' => 1,
+		  'interval_unit'   => 'month'
+		);
+
+		$sig = GoCardless_Utils::generate_signature($payment_details, $this->config['app_secret']);
+
+		// Check against a pre-built hash
+		$this->assertEquals('889b12a1aa31ca4c804d8554d4991fccc2d6d269bca4a20ecde6ef0cc20abc9c', $sig);
+
+	}
 
 }
