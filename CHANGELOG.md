d84455f8fd4746886cfe6cdd7765e40364d4198c
Alex Good <alexjsgood@gmail.com>
2012-08-02 15:42:52 +0100

    Bumping version number

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.3.3 - August 2, 2012
+
+- Bundled SSL certificates with library.
+
 ## 0.3.2 - May 28, 2012
 
 - Added debugging lines for SSL certificate issues
--- lib/GoCardless.php
+++ lib/GoCardless.php
@@ -29,7 +29,7 @@ class GoCardless {
    *
    * @var constant VERSION
    */
-  const VERSION = '0.3.2';
+  const VERSION = '0.3.3';
 
   /**
    * The environment: sandbox or live
